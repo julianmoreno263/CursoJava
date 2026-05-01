2bfb86b99eb81c7ff6c86572dd7bc5c5059cbbe5
julian <julianmoreno263@gmail.com>
2026-04-30 19:12:33 -0500

    Trabajo con strings y ejercicios

---
+++ 1-bases/Strings.java
@@ -0,0 +1,77 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        var name="Julian";
+
+        //concatenar
+        System.out.println("Hola" + " " + name);
+        
+        //longitud
+        System.out.println(name.length());
+
+        //obtener caracter, aqui obtiene el ultimo
+        System.out.println(name.charAt(name.length() -1));
+
+        //substring, es tomar un trozo del string
+        System.out.println(name.substring(2));
+
+        //mayusculas y minusculas
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        //comprobar si contiene algo especifico
+        System.out.println("Hola, Java".contains("Java"));
+
+        //comparacion entre cadenas
+        System.out.println(name.equals("Julian"));
+        System.out.println(name.equalsIgnoreCase("julian"));//aqui ignora mayusculas o minusculas
+
+        // == vs equals()
+        var a="julian";
+        var b="julian";
+        // var c=new Strings("julian");
+
+        System.out.println(a==b);//aqui da true porque java compara que son dos objetos iguales del sistema
+        // System.out.println(a==c);//aqui da false porque java dice que son objetos diferentes,uno creado con String
+
+        //aqui da true porque equals compara el valor y ve que son iguales, la buena practica es usar equals
+        // System.out.println(a.equals(c));
+
+        //trim(), elimina espacios al principio y al final
+        System.out.println("  Hola Julian, como estas?   ".trim());
+
+        //replace(), reemplaza caracteres,por ejemplo quiero reemplazar el nombre
+        System.out.println("  Hola Julian, como estas?   ".replace("Julian", "Pepe"));
+
+        //format, se pasa $s para cadenas, %d para enteros y %f para decimales
+        var age=34;
+        System.out.println(String.format("Hola %s, tienes %d años",name,age, args));
+
+        //ejercicios
+        // 1. Concatena dos cadenas de texto.
+
+        // 2. Muestra la longitud de una cadena de texto.
+
+        // 3. Muestra el primer y último carácter de un string.
+
+        // 4. Convierte a mayúsculas y minúsculas un string.
+
+        // 5. Comprueba si una cadena de texto contiene una palabra concreta.
+
+        // 6. Formatea un string con un entero.
+
+        // 7. Elimina los espacios en blanco al principio y final de un string.
+
+        // 8. Sustituye todos los espacios en blanco de un string por un guión (-).
+
+        // 9. Comprueba si dos strings son iguales.
+
+        // 10. Comprueba si dos strings tienen la misma longitud
+
+
+
+
+
+    }
+}
